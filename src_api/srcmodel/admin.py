2710958e0a43f4d9960953ff0f2212e4d85c5ebd
S_0127 <1476098078@qq.com>
2023-03-12 19:39:53 +0800

    表结构创建

--- src_api/srcmodel/admin.py
+++ src_api/srcmodel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from srcmodel.models import *
+ 
 # Register your models here.
+admin.site.register([CompanyInformation,Department, ManageUserDetail, UserDetail, User, 
+    ProjectType, IndustryType, SkillRequirements, BusinessType, RewardType, RewardLevel, ProjectStatus, Project, 
+    LeakRank, LeakInformation])
--- src_api/srcmodel/models.py
+++ src_api/srcmodel/models.py
@@ -5,9 +5,9 @@ from django.db import models
 #  1项目信息表
 class Project(models.Model):
     project_id = models.CharField(max_length=32) # 项目编号
-    project_name = models.DecimalField(max_digits=5, decimal_places=2) # 项目名称，
-    project_details = models.CharField(max_length=32) # 项目详情，
-    team_information = models.ForeignKey("TeamInformation", on_delete=models.CASCADE) # 所属团队，外键，多对一
+    project_name = models.CharField(max_length=32) # 项目名称，
+    project_details = models.TextField() # 项目详情，
+    company_information = models.ForeignKey("CompanyInformation", on_delete=models.CASCADE) # 所属公司，外键，多对一
     project_type = models.ForeignKey("ProjectType", on_delete=models.CASCADE) # 项目类型，外键，多对一
     industry_type = models.ForeignKey("IndustryType", on_delete=models.CASCADE) # 行业类型，外键，多对一
     skill_requirements = models.ManyToManyField("SkillRequirements") # 技能要求（多对多）
@@ -19,91 +19,42 @@ class Project(models.Model):
     start_data = models.DateField() # 项目开始时间，
     end_data = models.DateField() # 项目结束时间，
 
-# 2团队信息表
-class TeamInformation(models.Model):
-    team_name = models.CharField(max_length=32)
-    team_introduction = models.CharField(max_length=64)
-    logo = models.CharField(max_length=32)
-    address = models.CharField(max_length=32)
-    website = models.CharField(max_length=32)
-    email = models.EmailField()
-    phone = models.CharField(max_length=32)
-    password = models.CharField(max_length=64)
+# 2公司信息表
+class CompanyInformation(models.Model): 
+    company_name = models.CharField(max_length=32) # 团队名称，
+    company_introduction = models.TextField() # 团队介绍，
+    logo = models.CharField(max_length=32) # logo，
+    address = models.CharField(max_length=32) # 地址，
+    website = models.URLField() # 网站链接，
 
 # 3项目类型
 class ProjectType(models.Model):
-    type_name = (
-        (0, "普通项目"),
-        (1, "高级项目"),
-        (2, "企业SRC"),
-        (3, "攻防渗透"),
-        (4, "兼职测试"),
-    )
+    type_name = models.CharField(max_length=32)
 
 # 4行业类型
 class IndustryType(models.Model):
-    type_name = (
-        (0, "金融"),
-        (1, "电商"),
-        (2, "娱乐"),
-        (3, "运输"),
-        (4, "旅游"),
-        (5, "房产"),
-        (6, "制造业"),
-        (7, "政企"),
-        (8, "健康"),
-        (9, "互联网"),
-        (10, "综合"),
-        (11, "其他"),
-    )
+    type_name = models.CharField(max_length=32)
 
 # 5技能要求
 class SkillRequirements(models.Model):
-    type_name = (
-        (0, "web传统漏洞"),
-        (1, "APP安全漏洞"),
-        (2, "逻辑漏洞"),
-        (3, "情报/信息收集"),
-        (4, "内网渗透"),
-        (5, "云安全"),
-        (6, "代码审计"),
-        (7, "逆向破解"),
-        (8, "API测试"),
-        (9, "反序列化"),
-        (10, "二进制分析"),
-        (11, "C/S端测试"),
-        (12, "IOT测试"),
-        (13, "安全开发"),
-        (14, "应急溯源"),
-        (15, "CTF"),
-        (16, "其他"),
-    )
+    type_name = models.CharField(max_length=32)
 
 # 6业务类型
 class BusinessType(models.Model):
-    type_name = (
-        (0, "WEB端"),
-        (1, "移动客户端"),
-        (2, "PC端"),
-        (3, "智能硬件"),
-    )
+    type_name = models.CharField(max_length=32)
 
 # 7奖励类型
 class RewardType(models.Model):
-    type_name = (
-        (0, "奖金"),
-        (1, "代金卷"),
-        (2, "积分"),
-    )
+    type_name = models.CharField(max_length=32)
 
 # 8奖励等级
 class RewardLevel(models.Model):
     level_name = (
-        (0, "1"),
-        (1, "2"),
-        (2, "3"),
-        (3, "4"),
-        (3, "5"),
+        (0, "1级"),
+        (1, "2级"),
+        (2, "3级"),
+        (3, "4级"),
+        (4, "5级"),
     )
 # 9项目状态
 class ProjectStatus(models.Model):
@@ -114,20 +65,80 @@ class ProjectStatus(models.Model):
         (3, "已结束"),
     )
 
+# 漏洞信息表
+class LeakInformation(models.Model):
+    leak_id = models.CharField(max_length=32) # 漏洞编号，
+    leak_name = models.CharField(max_length=32) # 漏洞名称，
+    leak_describe = models.TextField() # 漏洞描述，
+    leak_scope = models.CharField(max_length=32) # 影响范围，
+    leak_harm = models.CharField(max_length=64) # 漏洞危害，
+    leak_rank = models.ForeignKey("LeakRank", on_delete=models.CASCADE) # 漏洞等级，多对一
+    repair_comments = models.CharField(max_length=64) # 整改意见，
+    leak_status = models.CharField(max_length=32) # 漏洞状态，
+    leak_reward = models.IntegerField() # 漏洞奖励，保留字段
+    create_data = models.DateField() # 创建时间，
+    author = models.ForeignKey("User", on_delete=models.CASCADE) # 提交人，多对一
 
-class Author(models.Model):
-    name = models.CharField(max_length=32)
-    age = models.SmallIntegerField()
-    au_detail = models.OneToOneField("AuthorDetail", on_delete=models.CASCADE)
+# 漏洞等级
+class LeakRank(models.Model):
+    level_name = (
+        (0, "紧急"),
+        (1, "高危"),
+        (2, "中危"),
+        (3, "低危"),
+    )
 
+# 用户表
+class User(models.Model):
+    username = models.CharField(max_length=32) # 用户名
+    email = models.EmailField() # 邮箱，
+    phone = models.CharField(max_length=32) # 联系电话，
+    password = models.CharField(max_length=254) # 认证密码，
+    usertype = (
+        (0, "安全人员"),
+        (1, "企业用户"),
+        (2, "运营人员"),
+    ) # 用户类型
+    user_detail = models.OneToOneField("UserDetail", on_delete=models.CASCADE) # 测试用户信息关联
+    company_information = models.OneToOneField("CompanyInformation", on_delete=models.CASCADE) # 企业用户信息关联
+    create_name = models.OneToOneField("ManageUserDetail", on_delete=models.CASCADE) # 运营用户信息关联
 
-class AuthorDetail(models.Model):
-    gender_choices = (
+# 运营用户信息表
+class ManageUserDetail(models.Model):
+    name = models.CharField(max_length=32) # 姓名
+    sex = (
         (0, "女"),
         (1, "男"),
         (2, "保密"),
     )
-    gender = models.SmallIntegerField(choices=gender_choices)
-    tel = models.CharField(max_length=32)
-    addr = models.CharField(max_length=64)
-    birthday = models.DateField()
+    birthday = models.DateField() # 生日
+    entry_data = models.DateField() # 入职时间
+    department = models.ForeignKey("Department", on_delete=models.CASCADE) # 所属部门
+
+
+# 部门表
+class Department(models.Model):
+    dep_name = models.CharField(max_length=32) # 部门名称
+
+# 安全测试用户信息
+class UserDetail(models.Model):
+    name = models.CharField(max_length=32) # 姓名
+    sex = (
+        (0, "女"),
+        (1, "男"),
+        (2, "保密"),
+    )
+    birthday = models.DateField() # 生日
+    diploma = models.CharField(max_length=32) # 学历
+    occupation = models.CharField(max_length=32) # 职业
+    introduce = models.CharField(max_length=254) #简介
+#     team_information = models.ForeignKey("TeamInformation", on_delete=models.CASCADE) # 所属团队，多对一
+
+# class TeamInformation(models.Model):
+#     team_name = models.CharField(max_length=32) # 团队名称
+#     team_describe = models.CharField(max_length=254) # 团队名称
+#     create_data = models.DateField() # 创建时间
+#     create_name = models.OneToOneField("UserDetail", on_delete=models.CASCADE) # 创建人
+#     team_leader = models.OneToOneField("UserDetail", on_delete=models.CASCADE) # 队长
+
+    
