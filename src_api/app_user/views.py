803e58c1c8049a8b8dcb786c56d59bdaee3f3c38
S_0127 <1476098078@qq.com>
2023-03-20 23:31:24 +0800

    登录接口token获取

--- src_api/app_user/views.py
+++ src_api/app_user/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework_simplejwt import authentication
+from app_user.auth import MyTokenObtainPairSerializer
+import json
  
 # Create your views here.
 
@@ -11,7 +13,9 @@ from rest_framework_simplejwt import authentication
 def ListShops(requests):
     return HttpResponse("this is shop list")
 def login(requests):
-    return HttpResponse("nologin")
+    token = MyTokenObtainPairSerializer.validate(MyTokenObtainPairSerializer,attrs={"username":requests.POST['username'],"password":requests.POST['password']})
+    print(token)
+    return JsonResponse(token)
  
 # 需要携带token才可以访问的视图 
 # Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc5MDcwMzg1LCJpYXQiOjE2NzkwNjk3ODUsImp0aSI6ImVjYTQxMDQ5ZmE2NzRkNDRiZDY3M2M2ZGEwMzMxN2U3IiwidXNlcl9pZCI6MX0.vF4jFSwP8QCQ1oO4igpNcw0lKmV1f6kbmTqdy5NFJiM
@@ -21,7 +25,7 @@ class ListView(APIView):
     authentication_classes = (authentication.JWTAuthentication,)
  
     def get(self, request, *args, **kwargs):
-        return Response('Get OK')
+        return HttpResponse('Get OK')
  
     def post(self, request, *args, **kwargs):
-        return Response('Post OK')
+        return HttpResponse('Post OK')
